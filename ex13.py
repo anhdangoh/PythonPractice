d0117c0b37ddddd3e077880d3bdb2bbd33d75384
anhdangoh <anh.dang@hotmail.co.uk>
2016-10-13 11:53:17 -0400

    Add ex13

---
+++ ex13.py
@@ -0,0 +1,8 @@
+from sys import argv
+
+script_name, first, second, third = argv
+
+print "The script is called: ", script_name
+print "Your first variable is: ", first
+print "Your second variable is: ", second
+print "Your third variable is: ", third
